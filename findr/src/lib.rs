4c380dacd2d7b01514d4057311675c6178ab47f5
Kwabena Boadu <shadrack.boadu@gmail.com>
2024-06-27 00:47:02 -0400

    refactor(findr): Refactor to iterator pattern

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -15,35 +15,42 @@ pub fn run<'b>() -> ProgramResult<'b> {
 }
 
 fn find_matches<'a>(args: &'a FindrArgs) -> ProgramResult<'static> {
-    for p in &args.paths {
-        for entry in WalkDir::new(p).into_iter().filter(|e| {
-            e.is_err()
-                || e.as_ref()
-                    .is_ok_and(|f| is_type_match(&f, args) && is_name_match(&f, args))
-        }) {
-            match entry {
-                Ok(entry) => println!("{}", entry.path().display()),
-                Err(e) => eprintln!("{}", translate_error(Box::new(e), p)),
-            }
-        }
-    }
+    args.paths.iter().for_each(|p| process_path(p, args));
     Ok(())
 }
 
-fn is_type_match(entry: &DirEntry, args: &FindrArgs) -> bool {
-    args.file_types.len() == 0
-        || args
-            .file_types
-            .iter()
-            .any(|f_type| f_type.is_type(&entry.file_type()))
-}
+fn process_path(p: &str, args: &FindrArgs) {
+    let type_filter = |entry: &DirEntry| -> bool {
+        args.file_types.is_empty()
+            || args
+                .file_types
+                .iter()
+                .any(|f_type| f_type.is_type(&entry.file_type()))
+    };
+    let name_filter = |entry: &DirEntry| -> bool {
+        args.names.is_empty()
+            || args
+                .names
+                .iter()
+                .any(|name| name.is_match(&entry.path().to_string_lossy()))
+    };
 
-fn is_name_match(entry: &DirEntry, args: &FindrArgs) -> bool {
-    args.names.len() == 0
-        || args
-            .names
-            .iter()
-            .any(|n| n.is_match(entry.path().to_str().unwrap()))
+    let out = WalkDir::new(p)
+        .into_iter()
+        .filter_map(|res| match res {
+            Err(e) => {
+                eprintln!("{}", translate_error(Box::new(e), p));
+                None
+            }
+            Ok(entry) => Some(entry),
+        })
+        .filter(type_filter)
+        .filter(name_filter)
+        .map(|entry| entry.path().display().to_string())
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    println!("{out}");
 }
 
 fn translate_error(e: Box<dyn error::Error>, pathname: &str) -> ProgramError {
